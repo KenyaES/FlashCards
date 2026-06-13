f7cd5d28a99d986f04ac8326ab15b2671cd706aa
Kenya Small <kenyaesmall@gmail.com>
2026-06-13 05:20:01 -0400

    Colors are set up and work to randomize

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const names = [
+    'Glaucous',
+    'Fulvous',
+    'Smalt Blue',
+    'Gamboge',
+    'Coquelicot',
+    'Zaffre',
+    'Isabelline',
+    'Aureolin',
+    'Caput Mortuum',
+    'Verdigris',
+    'Bole',
+    'Greige',
+    'Falu Red',
+    'Mikado Yellow',
+    'Amaranthine',
+    'Wenge',
+    'Celadon',
+    'Puce',
+    'Keppel',
+    'Byzantium',
+  ]
+  const colors = [
+    '#6082B6',
+    '#E48400',
+    '#51808F',
+    '#E49B0F',
+    '#FF3300',
+    '#0014A8',  
+    '#F4F0EC',
+    '#FDEE00',
+    '#592720',
+    '#5D5E37',
+    '#79443B',
+    '#CCC3B9',
+    '#801818',
+    '#FFC40C',
+    '#E52B50',
+    '#645452',
+    '#C3E0C6',
+    '#C3E0C6',
+    '#43B3A3',
+    '#702963',
+  ]
+  
+  const start = Math.floor(Math.random()*20)
+  const [name, setName] = useState(names[start])
+  const [color, setColor] = useState(colors[start])
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <div className="App">
+      <h1>Overly Specific Color Names</h1>
+      <h2>Try memorizing these obscure colors!</h2>
+      
+    </div>
   )
 }
 
